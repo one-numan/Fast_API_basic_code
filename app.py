b66334253c42c6d6a863598ed1c2587a27ea8e37
one-numan <numamk786@gmail.com>
2024-07-24 22:47:30 +0530

    Refactoring

--- app.py
+++ app.py
@@ -4,24 +4,13 @@ app = FastAPI()
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello, world!"}
-
-
-@app.get("/a")
-async def root1():
+async def home():
     return {"message": "Hello, world!"}
 
 
 @app.get('/book/{id}')
 async def book_data(id: int):
     return {"book": id}
-
-
-@app.route('/book/issue/{id}')
-async def book_issue(id: int):
-    return {"book_issue": id}
-
 
 if __name__ == "__main__":
     uvicorn.run(app, port=8000)
